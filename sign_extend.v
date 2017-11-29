b0b081a846a268f441b242571cbe499662dd6ea1
Bassem Mounir <bassem.mounir.elias@gmail.com>
2017-11-29 17:06:08 +0200

    sign extend module correction

--- sign_extend.v
+++ sign_extend.v
@@ -1,4 +1,5 @@
-module sign_extend(in , out)
-input reg in[15:0];
-output reg out[31:0];
-out<={{16{in[15]}} /*sign extend*/, in[15:0] /*16 bit address*/};endmodule
+module sign_extend(in,out);
+input [15:0] in;
+output [31:0] out;
+assign out={{16{in[15]}} /*sign extend*/, in[15:0] /*16 bit address*/};
+endmodule
