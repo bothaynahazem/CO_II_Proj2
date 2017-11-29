1f217bdb3235a5bdaef28fcdf2e0e0bcd2258115
Bassem Hossam <bassemhossam96@gmail.com>
2017-11-29 16:56:26 +0200

    comment.

--- MUX_3_to_1.v
+++ MUX_3_to_1.v
@@ -1,7 +1,9 @@
-module MUX_3_to_1(In1,In2,In3,out,mux_ctrl_2_bits)
+module MUX_3_to_1(In1,In2,In3,out,mux_ctrl);
 
-input [31:0] In1,In2,; 
-input mux_ctrl[1:0]; 
+input [31:0] In1;
+input [31:0]In2;
+input [31:0]In3; 
+input [1:0] mux_ctrl; 
 output reg[31:0] out;  
 
 always@(In1 or In2 or mux_ctrl) 
