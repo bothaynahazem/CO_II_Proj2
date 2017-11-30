7a4a5a232c184b7fa53fa7072fa1ce872d949cbb
Bassem Hossam <bassemhossam96@gmail.com>
2017-11-30 13:07:46 +0200

    singlecyclecpu complete

---
+++ PC.v
@@ -0,0 +1,12 @@
+module PC( clk,pc,pctoimem,PcP4);
+reg[31:0] PC=0;
+input clk;
+input[31:0]pc;
+output reg[31:0]pctoimem,PcP4;
+always@(posedge clk)
+begin
+pctoimem=PC;
+PcP4=pctoimem+4;
+PC<=pc;
+end
+endmodule
--- beqModule.v
+++ beqModule.v
@@ -1,11 +1,11 @@
-module beqModule (Branch, ZeroFlag, PcP4, Address, Pc,mux2in);
+module beqModule (Branch, ZeroFlag, PcP4, Address, pc,mux2in);
 
 input wire Branch;
 input wire ZeroFlag;
 input wire [31:0] PcP4;
 input wire [15:0] Address;
 
-output wire [31:0] Pc;
+output wire [31:0] pc;
 
 wire PcSrc;
 wire [31:0]sign_out;
@@ -17,6 +17,6 @@ assign PcSrc = Branch & ZeroFlag;
 sign_extend(Address,sign_out);
 assign add_in = sign_out<<1;
 assign add_out = PcP4 + add_in;
-MUX_2_to_1(PcP4,add_out,PcSrc,Pc);
+MUX_2_to_1(PcP4,add_out,PcSrc,pc);
 
 endmodule
