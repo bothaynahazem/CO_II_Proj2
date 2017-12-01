108957de794e8210974845addee434d518cef2b1
Bassem Hossam <bassemhossam96@gmail.com>
2017-12-02 01:22:10 +0200

    datamem now read in negclk and write in posclk

--- DataMem.v
+++ DataMem.v
@@ -1,14 +1,17 @@
 module DataMem(input clk,input [31:0] address,input [31:0] writedata,input MemWrite,input  MemRead,output reg[31:0] ReadData);
- reg[31:0] DMemory[0:1023];
+reg[31:0] DMemory[0:1023];
+always@(negedge clk)
+begin
+if(MemRead)
+begin
+ReadData=DMemory[address>>2];
+end
+end
 always@(posedge clk)
 begin
 if(MemWrite)
 begin
-DMemory[address>>2]<=writedata;
-end
-if(MemRead)
-begin
-ReadData<=DMemory[address>>2];
+DMemory[address>>2]=writedata;
 end
 end
 endmodule
